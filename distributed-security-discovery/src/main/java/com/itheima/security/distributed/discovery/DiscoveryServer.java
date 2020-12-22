5b61f0cd67671c5c5e638876b194cbf8af400845
yimao <m18845647584@163.com>
2020-12-22 23:15:15 +0800

    分布式oauth2.0验证

---
+++ distributed-security-discovery/src/main/java/com/itheima/security/distributed/discovery/DiscoveryServer.java
@@ -0,0 +1,14 @@
+package com.itheima.security.distributed.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class DiscoveryServer {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServer.class, args);
+    }
+}
